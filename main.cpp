71812c3b35b6b31c82750a95117888d0f0e06b66
jubaer.sookh <jubaer@sookh.com>
2022-08-29 17:08:27 +0600

    matrix add

--- main.cpp
+++ main.cpp
@@ -1,30 +1,48 @@
-
 #include <iostream>
 using namespace std;
 
 int main() {
-
-  //prime number between 1 and 100
-  int start;
-  int end;
-  cout << "Enter start number: ";
-  cin >> start;
-  cout << "Enter end number: ";
-  cin >> end;
-  for (int i = start; i <= end; i++) {
-    bool isPrime = true;
-    for (int j = 2; j < i; j++) {
-      if (i % j == 0) {
-        isPrime = false;
-        break;
-      }
-    }
-    if (isPrime) {
-      cout << i << endl;
+  //matrix addition
+  int matrix1[100][100], matrix2[100][100], result[100][100], r1, c1, r2, c2;
+  cout << "Enter rows and columns of first matrix :";
+  cin >> r1 >> c1;
+  cout << "Enter rows and columns of second matrix :";
+  cin >> r2 >> c2;
+  if (c1 != r2) {
+    cout << "Addition not possible";
+    return 0;
+  }
+  cout << "Enter elements of first matrix :" << endl;
+  for (int i = 0; i < r1; i++) {
+    for (int j = 0; j < c1; j++) {
+      cout << "Enter element [" << i << "][" << j << "] :";
+      cin >> matrix1[i][j];
     }
   }
-  
-  
+  cout << "Enter elements of second matrix :" << endl;
+  for (int i = 0; i < r2; i++) {
+    for (int j = 0; j < c2; j++) {
+      cout << "Enter element [" << i << "][" << j << "] :";
+      cin >> matrix2[i][j];
+    }
+  }
+  for (int i = 0; i < r1; i++) {
+    for (int j = 0; j < c2; j++) {
+      
+      result[i][j] = matrix1[i][j] + matrix2[i][j];
+    }
+  }
+  cout << "Addition of two matrices is :" << endl;
+  for (int i = 0; i < r1; i++) {
+    for (int j = 0; j < c2; j++) {
+      cout << result[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+
 
   return 0;
+
+
 }
