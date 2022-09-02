c04d9b4515a61765edc54fba532830a9c32fddb8
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-02 19:10:57 +0600

    leetcode pascal triangle problem

--- main.cpp
+++ main.cpp
@@ -1,36 +1,26 @@
 
 #include <iostream>
-#include <math.h>
+#include <vector>
 using namespace std;
 
-
-int factorial(int r){
-    int fact = 1;
-    for(int i = 2; i <= r; i++){
-        fact = fact * i;
-    }
-    return fact;
-}
 
 
 int main()
 {
-   int n;
-   cout << "Enter a number: ";
-   cin >> n;
 
-   // pascal's triangle
 
-   for (int i = 0; i < n; i++)
-   {
-    for (int j = 0; j <= i; j++)
-    {
-           int nCr = factorial(i) / (factorial(j) * factorial(i - j));
-           cout << nCr << " ";
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1, 1);
+            for (int j = 1; j < i; j++) {
+                row[j] = result[i - 1][j - 1] + result[i - 1][j];
+            }
+            result.push_back(row);
+        }
+        return result;
     }
-    cout << endl;
-    
-   }
    
 
 
