cced3c061459f8268a979b653c536b2c9219dfaa
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-02 10:46:00 +0600

    butter fly

--- main.cpp
+++ main.cpp
@@ -2,21 +2,44 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
-  int n;
-  cout << "Enter start number: ";
-  cin >> n;
-  for (int i = 1; i <= n; i++)
-  {
-     for (int j = 1; j <= i; j++)
-     {
-        cout << j << " ";
-     }
-     cout << endl;
-  }
-  
-  
+   int n;
+   cout << "Enter start number: ";
+   cin >> n;
+   for (int i = 1; i <= n; i++)
+   {
+      for (int j = 1; j <= i; j++)
+      {
+         cout << "*";
+      }
+      for (int j = 1; j <= (2 * n - 2 * i); j++)
+      {
+         cout << " ";
+      }
+      for (int j = 1; j <= i; j++)
+      {
+         cout << "*";
+      }
+      cout << endl;
+   }
+   for (int i = n; i >= 1; i--)
+   {
+      for (int j = 1; j <= i; j++)
+      {
+         cout << "*";
+      }
+      for (int j = 1; j <= (2 * n - 2 * i); j++)
+      {
+         cout << " ";
+      }
+      for (int j = 1; j <= i; j++)
+      {
+         cout << "*";
+      }
+      cout << endl;
+   }
 
-  return 0;
+   return 0;
 }
