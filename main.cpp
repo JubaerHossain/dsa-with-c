4c6b134680f70faa1e91a0694a75ac8dd38c7169
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-02 10:44:35 +0600

    s

--- main.cpp
+++ main.cpp
@@ -4,24 +4,16 @@ using namespace std;
 
 int main() {
 
-  //prime number between 1 and 100
-  int start;
-  int end;
+  int n;
   cout << "Enter start number: ";
-  cin >> start;
-  cout << "Enter end number: ";
-  cin >> end;
-  for (int i = start; i <= end; i++) {
-    bool isPrime = true;
-    for (int j = 2; j < i; j++) {
-      if (i % j == 0) {
-        isPrime = false;
-        break;
-      }
-    }
-    if (isPrime) {
-      cout << i << endl;
-    }
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+  {
+     for (int j = 1; j <= i; j++)
+     {
+        cout << j << " ";
+     }
+     cout << endl;
   }
   
   
