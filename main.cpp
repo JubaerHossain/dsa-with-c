d56f450706b2dcbdedb4ad6b7e5ea4d12be468d6
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-02 19:12:16 +0600

    leetcode pascal triangle problem

--- main.cpp
+++ main.cpp
@@ -3,28 +3,26 @@
 #include <vector>
 using namespace std;
 
-
-
 int main()
 {
 
-
-
-    vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> generate(int numRows)
+    {
         vector<vector<int>> result;
-        for (int i = 0; i < numRows; i++) {
+        for (int i = 0; i < numRows; i++)
+        {
             vector<int> row(i + 1, 1);
-            for (int j = 1; j < i; j++) {
+            for (int j = 1; j < i; j++)
+            {
                 row[j] = result[i - 1][j - 1] + result[i - 1][j];
             }
             result.push_back(row);
         }
         return result;
-    }
-   
+    };
 
 
+    
 
-
-   return 0;
+    return 0;
 }
