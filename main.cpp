eca24d26a86633f328f299d4462a5e720b0693ab
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-02 11:43:57 +0600

    prime number specific or between

--- main.cpp
+++ main.cpp
@@ -1,30 +1,57 @@
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+int main()
+{
 
-  //prime number between 1 and 100
-  int start;
-  int end;
-  cout << "Enter start number: ";
-  cin >> start;
-  cout << "Enter end number: ";
-  cin >> end;
-  for (int i = start; i <= end; i++) {
-    bool isPrime = true;
-    for (int j = 2; j < i; j++) {
-      if (i % j == 0) {
-        isPrime = false;
-        break;
+   // specific number prime or not
+   int num;
+   cout << "Enter a number: ";
+   cin >> num;
+   bool isPrime = true;
+
+   for (int i = 2; i < sqrt(num); i++)
+   {
+      if (num % i == 0)
+      {
+         cout << "Not Prime" << endl;
+         isPrime = false;
+         break;
       }
-    }
-    if (isPrime) {
-      cout << i << endl;
-    }
-  }
-  
-  
+      
+      
+   }
+   if (isPrime)
+   {
+      cout << "Prime" << endl;
+   }
+   
 
-  return 0;
+   // prime number between 1 and 100
+   int start;
+   int end;
+   cout << "Enter start number: ";
+   cin >> start;
+   cout << "Enter end number: ";
+   cin >> end;
+   for (int i = start; i <= end; i++)
+   {
+      bool isPrime = true;
+      for (int j = 2; j < i; j++)
+      {
+         if (i % j == 0)
+         {
+            isPrime = false;
+            break;
+         }
+      }
+      if (isPrime)
+      {
+         cout << i << endl;
+      }
+   }
+
+   return 0;
 }
