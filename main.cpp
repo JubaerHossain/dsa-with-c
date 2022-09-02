59bd6a882ad3842de83b9bf6745aab67b78d7cf0
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-02 16:05:14 +0600

    pascall triangle solve in leetcode

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-int ncr()
+
+
+int factorial(int r){
+    int fact = 1;
+    for(int i = 2; i <= r; i++){
+        fact = fact * i;
+    }
+    return fact;
+}
 
 
 int main()
@@ -12,6 +20,18 @@ int main()
    cin >> n;
 
    // pascal's triangle
+
+   for (int i = 0; i < n; i++)
+   {
+    for (int j = 0; j <= i; j++)
+    {
+           int nCr = factorial(i) / (factorial(j) * factorial(i - j));
+           cout << nCr << " ";
+    }
+    cout << endl;
+    
+   }
+   
 
 
 
