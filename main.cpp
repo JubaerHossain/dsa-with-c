462edcf1267dcb4506cfa709684248f434fdda3f
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-02 12:11:48 +0600

    armstrong number

--- main.cpp
+++ main.cpp
@@ -1,28 +1,35 @@
 
 #include <iostream>
-#include <cmath>
+#include <math.h>
 using namespace std;
 
 int main()
 {
-
-   // specific number prime or not
    int n;
    cout << "Enter a number: ";
    cin >> n;
 
-   // reverse numbers
-   int rev = 0;
-   int temp = n;
-   while (temp != 0)
+   // armstrong number
+
+   int sum = 0;
+   int originalN = n;
+   while (n > 0)
    {
-      int lastDigit = temp % 10;
-      rev = rev * 10 + lastDigit;
-      temp = temp / 10;
+      int lastDigit = n % 10;
+      int s = pow(lastDigit, 3);
+      sum += s;
+      n = n / 10;
    }
 
-   cout << "Reverse of " << n << " is " << rev << endl;
-   
+
+   if (sum == originalN)
+   {
+      cout << "Armstrong number";
+   }
+   else
+   {
+      cout << "Not an Armstrong number";
+   }
 
    return 0;
 }
