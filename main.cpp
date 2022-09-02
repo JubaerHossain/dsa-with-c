4bb996c067f28179de3eb944ee62bfa630d16f44
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-02 12:33:20 +0600

    fibonacci series

--- main.cpp
+++ main.cpp
@@ -3,20 +3,40 @@
 #include <math.h>
 using namespace std;
 
-int factorial(int n) {
-    if (n == 0) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+int fibonacci(int n)
+{
+   if (n == 0)
+   {
+      return 0;
+   }
+   else if (n == 1)
+   {
+      return 1;
+   }
+   else
+   {
+      int t1 = 0, t2 = 1, nextTerm = 0;
+
+      for (int i = 1; i <= n; i++)
+      {
+         cout << t1 << ", ";
+         nextTerm = t1 + t2;
+         t1 = t2;
+         t2 = nextTerm;
+      }
+
+      return 0;
+   }
 }
 
 int main()
 {
+
    int n;
    cout << "Enter a number: ";
    cin >> n;
-
-   cout << "Factorial of " << n << " is " << factorial(n);
+   cout << "Fibonacci Series: " << endl;
+   fibonacci(n);
 
    return 0;
 }
