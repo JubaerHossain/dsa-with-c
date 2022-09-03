30b6f0a6bc3c0aab7c01e6436e4d5cf9a0ae4f0e
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-03 13:32:20 +0600

    's'

--- main.cpp
+++ main.cpp
@@ -7,11 +7,9 @@ using namespace std;
 int main()
 {
     vector<int> g1;
+    g1.assign(5, 10);
 
-    for (int i = 1; i <= 5; i++)
-        g1.push_back(i);
 
-        
 
     cout << "Output of begin and end: ";
     for (auto i = g1.begin(); i != g1.end(); ++i)
