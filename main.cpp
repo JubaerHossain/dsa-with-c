70d31c25b39298a19bedcf4294b2a2c465acb3cf
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-28 22:30:29 +0600

    chaining_hash_table

--- main.cpp
+++ main.cpp
@@ -1,186 +1,66 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std; 
 
-class CircularQueue
-{
-    int *arr;
-    int front;
-    int rear;
-    int capacity;
-
-public:
-    CircularQueue(int capacity)
-    {
-        this->capacity = capacity;
-        arr = new int[capacity];
-        front = -1;
-        rear = -1;
-    }
-
-    bool isEmpty()
-    {
-        return front == rear;
-    }
-
-    bool isFull()
-    {
-        return rear + 1 == capacity;
-    }
-
-    void enqueue(int data)
-    {
-        if (isFull())
-        {
-            cout << "Queue is full" << endl;
-            return;
-        }
-        arr[rear] = data;
-        rear = (rear + 1) % capacity;
-    }
-
-    int dequeue()
-    {
-        if (isEmpty())
-        {
-            cout << "Queue is empty" << endl;
-            return -1;
-        }
-        int data = arr[front];
-        front = (front + 1) % capacity;
-        return data;
-    }
-
-    void peak()
-    {
-        if (isEmpty())
-        {
-            cout << "Queue is empty" << endl;
-            return;
-        }
-        cout << arr[front] << endl;
-    }
-
-    void print()
-    {
-        if (isEmpty())
-        {
-            cout << "Queue is empty" << endl;
-            return;
-        }
-        cout << "Queue is: ";
-        for (int i = front; i < rear; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
-};
-
-class Queue
-{
-    int *arr;
-    int capacity;
-    int front,rear;
+class HashTable {
+    private:
+    int n;
+    list<int> *table;
 
     public:
-    Queue(int capacity)
-    {
-        this->capacity = capacity;
-        arr = new int[capacity];
-        front = -1;
-        rear = -1;
-    }
+    HashTable(int n) ;
 
-    bool isEmpty()
-    {
-        return front == rear;
-    }
-    bool isFull()
-    {
-        return rear + 1 == capacity;
-    }
+    void insert(int key) ;
 
-    void enqueue(int data)
-    {
-        if(isFull())
-        {
-            cout << "Queue is full" << endl;
-            return;
-        }
-        arr[rear++] = data;
-    }
+    void remove(int key) ;
 
-    int dequeue()
-    {
-        if(isEmpty())
-        {
-            cout << "Queue is empty" << endl;
-            return -1;
-        }
-        return arr[front++];
+    bool search(int key) ;
 
-    }
+    void display() ;
+};
 
-    void peak()
-    {
-        if(isEmpty())
-        {
-            cout << "Queue is empty" << endl;
-            return;
-        }
-        cout << arr[front] << endl;
-    }
+HashTable::HashTable(int n) {
+    this->n = n;
+    table = new list<int>[n];
+}
 
-    void print()
-    {
-        if(isEmpty())
-        {
-            cout << "Queue is empty" << endl;
-            return;
-        }
-        cout << "Queue is: ";
-        for(int i = front; i < rear; i++)
-        {
-            cout << arr[i] << " ";
-        }
+void HashTable::insert(int key) {
+    int i = key % n;
+    table[i].push_back(key);
+}
+
+void HashTable::remove(int key) {
+    int i = key % n;
+    table[i].remove(key);
+}
+
+bool HashTable::search(int key) {
+    int i = key % n;
+    for (auto x : table[i])
+        if (x == key)
+            return true;
+    return false;
+}
+
+void HashTable::display() {
+    for (int i = 0; i < n; i++) {
+        cout << i;
+        for (auto x : table[i])
+            cout << " --> " << x;
         cout << endl;
     }
-};
+}
+
+
+
 int main()
 {
+    vector<int> keys = {4, 6, 9, 12, 34};
+    HashTable h(10);
+    for (int key : keys)
+        h.insert(key);
 
+    h.display();
 
-
-    int n = 5;
-
-    Queue q(n);
-
-    q.enqueue(1);
-    q.enqueue(2);
-    q.enqueue(3);
-    q.enqueue(4);
-    q.enqueue(5);
-
-    q.print();
-
-    cout << "Circular Queue are :   " << endl;
-    CircularQueue cq(n);
-    cq.enqueue(10);
-    cq.enqueue(20);
-    cq.enqueue(30);
-    cq.enqueue(40);
-    cq.enqueue(50);
-    cq.enqueue(60);
-    cq.print();
-    cout << "peak value is : ";
-    cq.peak();
-
-    // cq.dequeue();
-    // cout << "dequeue" << endl;
-    // cq.print();
-    // cq.enqueue(60);
-    // cout << "after enqueue" << endl;
-    // cq.print();
-
+    
     return 0;
 }
