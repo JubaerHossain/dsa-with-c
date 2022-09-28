af40eca487e4328da1e39e6eb3dcdb834acb7add
JubaerHossain <jubaer01.cse@gmail.com>
2022-09-28 23:01:25 +0600

    binery serch

--- main.cpp
+++ main.cpp
@@ -1,49 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
-class LinearHash{
-    private:
-    list<int> *table;
-
-    public:
-    LinearHash(int size){
-        table = new list<int>[size];
-    }
-
-    void insert(int key){
-        int index = key % 10;
-        table[index].push_back(key);
-    }
-
-    void remove(int key){
-        int index = key % 10;
-        table[index].remove(key);
-    }
-
-    bool search(int key){
-        int index = key % 10;
-        for(auto i = table[index].begin(); i != table[index].end(); i++){
-            if(*i == key){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void display(){
-        for(int i = 0; i < 10; i++){
-            cout << i;
-            for(auto j = table[i].begin(); j != table[i].end(); j++){
-                cout << " --> " << *j;
-            }
-            cout << endl;
-        }
-    }
-
-
-
-};
-
 int main()
 {
     
